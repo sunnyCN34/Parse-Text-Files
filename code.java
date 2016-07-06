4f31b9a7ee3c1515751de96e775dbfe1ed424035
sunnysahu34 <sunny.sahu@ten24web.com>
2016-07-06 09:10:54 +0530

    Add files via upload

--- code.java
+++ code.java
@@ -5,7 +5,7 @@ class mycode
 {
  public static void main(String args[])
   {
- int count=0; int count2=0;
+ int count=0; int count2=0; double percent=0;
   try
    {
   File f1= new File("test file.txt");
@@ -19,11 +19,29 @@ class mycode
           }
 
      }
-System.out.println("word appears= "+count+" times in text file");
-    } 
-   catch(Exception e)
+System.out.println("word appears= "+count+" times in first text file");
+   
+File f2= new File("test file2.txt");
+Scanner s2= new Scanner(f2);
+   while(s2.hasNext())
     {
-   System.out.println(e);
+   String word2= s2.next();
+       if(word2.equals("wizards"))
+          {
+         count2++;
+          }
+     }
+ System.out.println("word appears= "+count2+" times in second text file");
+ System.out.println("count="+count+" count2="+count2);
+     
+      percent= (count*100)/count2;
+
+ System.out.println("Percentage of test coverage we have is equal to= "+percent); 
+   } 
+       catch(Exception e)
+         {
+        System.out.println(e);
+         }
+ 
     }
-  }
 }
