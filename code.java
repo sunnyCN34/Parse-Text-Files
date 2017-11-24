44fbb5fc09fbda2c24cd2e7cd96078c26c6c85c7
sunnyCN34 <ssahu@brightcove.com>
2017-11-24 17:11:24 -0500

    format the code

--- code.java
+++ code.java
@@ -1,47 +1,40 @@
 import java.util.*;
 import java.util.Scanner;
 import java.io.File;
-class mycode
-{
- public static void main(String args[])
-  {
- int count=0; int count2=0; double percent=0;
-  try
-   {
-  File f1= new File("test file.txt");
-  Scanner s1=new Scanner(f1);
-    while(s1.hasNext())
-     {
-    String word=s1.next();
-       if(word.equals("function"))
-          {
-           count++;
-          }
+class mycode {
+    public static void main(String args[]) {
+        int count = 0;
+        int count2 = 0;
+        double percent = 0;
+        try {
+            File f1 = new File("test file.txt");
+            Scanner s1 = new Scanner(f1);
+            while (s1.hasNext()) {
+                String word = s1.next();
+                if (word.equals("function")) {
+                    count++;
+                }
 
-     }
-System.out.println("word appears= "+count+" times in first text file");
-   
-File f2= new File("test file2.txt");
-Scanner s2= new Scanner(f2);
-   while(s2.hasNext())
-    {
-   String word2= s2.next();
-       if(word2.equals("function"))
-          {
-         count2++;
-          }
-     }
- System.out.println("word appears= "+count2+" times in second text file");
- System.out.println("count="+count+" count2="+count2);
-     
-      percent= (count*100)/count2;
+            }
+            System.out.println("word appears= " + count + " times in first text file");
 
- System.out.println("Percentage of test coverage we have is equal to= "+percent+"%"); 
-   } 
-       catch(Exception e)
-         {
-        System.out.println(e);
-         }
- 
+            File f2 = new File("test file2.txt");
+            Scanner s2 = new Scanner(f2);
+            while (s2.hasNext()) {
+                String word2 = s2.next();
+                if (word2.equals("function")) {
+                    count2++;
+                }
+            }
+            System.out.println("word appears= " + count2 + " times in second text file");
+            System.out.println("count=" + count + " count2=" + count2);
+
+            percent = (count * 100) / count2;
+
+            System.out.println("Percentage of test coverage we have is equal to= " + percent + "%");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
